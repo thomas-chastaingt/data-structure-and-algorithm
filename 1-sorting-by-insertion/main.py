c44724feed677e8ca050ffdc546ce519edd2d447
Thomas Chastaingt <thomas-chastaingt@outlook.com>
2020-09-02 11:55:55 +0200

    add sorting by insertion

--- 1-sorting-by-insertion/main.py
+++ 1-sorting-by-insertion/main.py
@@ -1,3 +1,14 @@
-tab = [5,2,4,6,1,3]
+def sorting(tab):
+    for j in range(1,len(tab)): 
+        key = tab[j]         
+        i = j - 1
+        while i>=0 and tab[i] > key: 
+            tab[i+1] = tab[i]    
+            i=i-1
+        tab[i+1] = key
 
-print(tab)
+tab = [98, 22, 15, 32, 2, 74, 63, 70]
+sorting(tab)
+print("Le tableau trié est : ")
+for i in range(len(tab)):
+    print("% d" % tab[i])
